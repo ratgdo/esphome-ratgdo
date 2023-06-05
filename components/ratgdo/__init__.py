49a4f0896095ff3dea753f812a13e354a03e5d1f
J. Nick Koston <nick@koston.org>
2023-06-05 12:18:26 -0500

    adjust

--- components/ratgdo/__init__.py
+++
@@ -1,7 +0,0 @@
-import esphome.codegen as cg import esphome.config_validation as cv from esphome.core import coroutine_with_priority
-
-    ratgdo_ns = cg.esphome_ns.namespace("ratgdo")
-
-                                            CONFIG_SCHEMA = cv.All(cv.Schema({}), )
-
-                                                                       @coroutine_with_priority(1.0) async def to_code(config) :cg.add_library("bblanchon/ArduinoJson", "6.18.5") cg.add_define("USE_JSON") cg.add_global(ratgdo_ns.using)
