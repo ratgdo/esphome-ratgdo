33d1f3a091b23c6548f1235bbabdb73db62b4348
J. Nick Koston <nick@koston.org>
2023-06-05 13:42:39 -0500

    fix

--- components/ratgdo/ratgdo.h
+++ components/ratgdo/ratgdo.h
@@ -14,13 +14,25 @@
 #pragma once
 #include "esphome/core/component.h"
 #include "esphome/core/preferences.h"
-#include <Arduino.h>
 
 
 #include "SoftwareSerial.h"
 extern "C" {
 #include "secplus.h"
 }
+
+static const uint8_t D0   = 16;
+static const uint8_t D1   = 5;
+static const uint8_t D2   = 4;
+static const uint8_t D3   = 0;
+static const uint8_t D4   = 2;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 15;
+static const uint8_t D9   = 3;
+static const uint8_t D10  = 1;
+
 
 #define CODE_LENGTH 19 // the length of each command sent to the door.
 /********************************** PIN DEFINITIONS
