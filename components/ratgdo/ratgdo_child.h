3d9bdd5897ddf6cfee44364ef187db572f8d4298
J. Nick Koston <nick@koston.org>
2023-06-07 10:02:53 -0500

    get rid of strings

--- components/ratgdo/ratgdo_child.h
+++ components/ratgdo/ratgdo_child.h
@@ -8,20 +8,15 @@ namespace ratgdo {
 
     // Forward declare RATGDOComponent
     class RATGDOComponent;
-    // Forward declare enums
-    enum DoorState;
-    enum LightState;
-    enum LockState;
-    enum MotionState;
-    enum ObstructionState;
+
 
     class RATGDOClient : public Parented<RATGDOComponent> {
     public:
-        virtual void on_door_state(DoorState state) = 0;
-        virtual void on_light_state(LightState state) = 0;
-        virtual void on_lock_state(LockState state) = 0;
-        virtual void on_motion_state(MotionState state) = 0;
-        virtual void on_obstruction_state(ObstructionState state) = 0;
+        virtual void on_door_state(RATGDOComponent::DoorState state) = 0;
+        virtual void on_light_state(RATGDOComponent::LightState state) = 0;
+        virtual void on_lock_state(RATGDOComponent::LockState state) = 0;
+        virtual void on_motion_state(RATGDOComponent::MotionState state) = 0;
+        virtual void on_obstruction_state(RATGDOComponent::ObstructionState state) = 0;
 
     protected:
         friend RATGDOComponent;
