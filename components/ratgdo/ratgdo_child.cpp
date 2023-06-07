f5e1e72e25938c8849f49d840207fbe156b5af19
J. Nick Koston <nick@koston.org>
2023-06-07 17:54:31 -0500

    tweak

---
+++ components/ratgdo/ratgdo_child.cpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "esphome/core/helpers.h"
+
+#include "ratgdo.h"
+#include "ratgdo_state.h"
+
+namespace esphome {
+namespace ratgdo {
+
+    void RATGDOClient::on_door_state(DoorState state) {};
+    void RATGDOClient::on_light_state(LightState state) {};
+    void RATGDOClient::on_lock_state(LockState state) {};
+    void RATGDOClient::on_motion_state(MotionState state) {};
+    void RATGDOClient::on_obstruction_state(ObstructionState state) {};
+
+} // namespace ratgdo
+} // namespace esphome
