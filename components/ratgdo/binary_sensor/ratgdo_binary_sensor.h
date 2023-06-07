dc33ae2d3cbd224028d81f1476f64dd6067d5345
J. Nick Koston <nick@koston.org>
2023-06-07 14:49:22 -0500

    fix

--- components/ratgdo/binary_sensor/ratgdo_binary_sensor.h
+++ components/ratgdo/binary_sensor/ratgdo_binary_sensor.h
@@ -17,7 +17,7 @@ namespace ratgdo {
     class RATGDOBinarySensor : public binary_sensor::BinarySensor, public RATGDOClient, public Component {
     public:
         void dump_config() override;
-        void set_type(SensorType binary_sensor_type_) { this->binary_sensor_type_ = binary_sensor_type_; }
+        void set_binary_sensor_type(SensorType binary_sensor_type_) { this->binary_sensor_type_ = binary_sensor_type_; }
 
         void on_motion_state(MotionState state) override;
         void on_obstruction_state(ObstructionState state) override;
