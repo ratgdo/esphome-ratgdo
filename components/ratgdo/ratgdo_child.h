b42b5f423953419342717d960b529c80120984b5
J. Nick Koston <nick@koston.org>
2023-06-07 09:49:24 -0500

    get rid of strings

---
+++ components/ratgdo/ratgdo_child.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "esphome/core/helpers.h"
+
+namespace esphome {
+namespace ratgdo {
+
+// Forward declare RATGDOComponent
+class RATGDOComponent;
+
+class BedJetClient : public Parented<RATGDOComponent> {
+ public:
+  virtual void on_status() = 0;
+  virtual void on_ratgdo_state(bool is_ready) = 0;
+
+ protected:
+  friend RATGDOComponent;
+  virtual std::string describe() = 0;
+};
+
+}  // namespace ratgdo
+}  // namespace esphome
+
