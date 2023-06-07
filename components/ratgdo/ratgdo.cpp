d5cdc36615468a6553605bc0c60d03ed00c7a2d5
J. Nick Koston <nick@koston.org>
2023-06-07 10:00:48 -0500

    get rid of strings

--- components/ratgdo/ratgdo.cpp
+++ components/ratgdo/ratgdo.cpp
@@ -486,7 +486,7 @@ namespace ratgdo {
     {
         DoorState val = static_cast<DoorState>(this->store_.doorState);
         ESP_LOGD(TAG, "Door state: %s", door_state_to_string(val));
-        for (auto *child : this->children_) {
+        for (auto* child : this->children_) {
             child->on_door_state(val);
         }
         this->status_door_pin_->digital_write(this->store_.doorState == 1);
