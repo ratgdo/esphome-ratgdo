b481e70affdae818434b4e6a5ceba995acf38152
J. Nick Koston <nick@koston.org>
2023-06-07 12:02:26 -0500

    binary sensor

--- components/ratgdo/cover/__init__.py
+++ components/ratgdo/cover/__init__.py
@@ -17,7 +17,7 @@ RATGDOCover = ratgdo_ns.class_(
 )
 
 
-CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({cv.GenerateID(): cv.declare_id(RATGDO)}).extend(RATGDO_CLIENT_SCHMEA)
+CONFIG_SCHEMA = cover.COVER_SCHEMA.extend({cv.GenerateID(): cv.declare_id(RATGDOCover)}).extend(RATGDO_CLIENT_SCHMEA)
 
 
 async def to_code(config):
