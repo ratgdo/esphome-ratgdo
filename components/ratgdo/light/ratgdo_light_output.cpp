a360ebae77e6d20fa0445cadb8c404d19633a006
J. Nick Koston <nick@koston.org>
2023-06-09 16:45:53 -0500

    loop

--- components/ratgdo/light/ratgdo_light_output.cpp
+++ components/ratgdo/light/ratgdo_light_output.cpp
@@ -21,38 +21,39 @@ namespace ratgdo {
         }
     }
     void RATGDOLightOutput::set_state(esphome::ratgdo::LightState state)
-    
-        bool is_on = state == LightState::LIGHT_STATE_ON;
-        this->light_state_->current_values.set_state(is_on);
-        this->light_state_->remote_values.set_state(is_on);
-        this->light_state_->publish_state();
-    }
-    void RATGDOLightOutput::setup_state(light::LightState* light_state)
-    {
-        esphome::ratgdo::LightState state = this->parent_->getLightState();
-        ESP_LOGD(TAG,"setup_state: getLightState: %d", state)
-        this->light_state_ = light_state;
-        this->set_state(state);
-    }
-    LightTraits RATGDOLightOutput::get_traits()
-    {
-        auto traits = LightTraits();
-        traits.set_supported_color_modes({ light::ColorMode::ON_OFF });
-        return traits;
-    }
 
-    void RATGDOLightOutput::write_state(light::LightState* state)
-    {
-        bool binary;
-        state->current_values_as_binary(&binary);
-        if (binary) {
-            ESP_LOGD(TAG, "output call lightOn");
-            this->parent_->lightOn();
-        } else {
-            ESP_LOGD(TAG, "output call lightOff");
-            this->parent_->lightOff();
-        }
+        bool is_on
+        = state == LightState::LIGHT_STATE_ON;
+    this->light_state_->current_values.set_state(is_on);
+    this->light_state_->remote_values.set_state(is_on);
+    this->light_state_->publish_state();
+}
+void RATGDOLightOutput::setup_state(light::LightState* light_state)
+{
+    esphome::ratgdo::LightState state = this->parent_->getLightState();
+    ESP_LOGD(TAG, "setup_state: getLightState: %d", state)
+    this->light_state_ = light_state;
+    this->set_state(state);
+}
+LightTraits RATGDOLightOutput::get_traits()
+{
+    auto traits = LightTraits();
+    traits.set_supported_color_modes({ light::ColorMode::ON_OFF });
+    return traits;
+}
+
+void RATGDOLightOutput::write_state(light::LightState* state)
+{
+    bool binary;
+    state->current_values_as_binary(&binary);
+    if (binary) {
+        ESP_LOGD(TAG, "output call lightOn");
+        this->parent_->lightOn();
+    } else {
+        ESP_LOGD(TAG, "output call lightOff");
+        this->parent_->lightOff();
     }
+}
 
 } // namespace ratgdo
 } // namespace esphome
