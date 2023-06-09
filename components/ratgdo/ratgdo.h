04e712b3b8c43b7ad5783201224bb84b81105060
J. Nick Koston <nick@koston.org>
2023-06-09 15:16:55 -0500

    fix

--- components/ratgdo/ratgdo.h
+++ components/ratgdo/ratgdo.h
@@ -36,7 +36,7 @@ namespace ratgdo {
     typedef struct {
         uint64_t fixed;
         uint32_t data;
-        bool operator!=(const command a) const
+        inline bool operator!=(const command &a) const
         {
             return (fixed != a.fixed || data != a.data);
         }        
