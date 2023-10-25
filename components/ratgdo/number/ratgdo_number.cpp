269d715ce28d77e90d94169ae9c7161680d6d401
J. Nick Koston <nick@koston.org>
2023-10-25 12:54:34 -0500

    tidy

--- components/ratgdo/number/ratgdo_number.cpp
+++ components/ratgdo/number/ratgdo_number.cpp
@@ -12,7 +12,7 @@ namespace ratgdo {
         LOG_NUMBER("", "RATGDO Number", this);
         if (this->number_type_ == RATGDO_CLIENT_ID) {
             ESP_LOGCONFIG(TAG, " Type: Client ID");
-        }else if (this->number_type_ == RATGDO_ROLLING_CODE_COUNTER) {
+        } else if (this->number_type_ == RATGDO_ROLLING_CODE_COUNTER) {
             ESP_LOGCONFIG(TAG, "  Type: Rolling Code Counter");
         } else if (this->number_type_ == RATGDO_OPENING_DURATION) {
             ESP_LOGCONFIG(TAG, "  Type: Opening Duration");
@@ -26,20 +26,20 @@ namespace ratgdo {
         float value;
         this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
         if (!this->pref_.load(&value)) {
-            if(this->number_type_ == RATGDO_CLIENT_ID){
-                value = random(0x1,0xFFFF);
-            }else{
+            if (this->number_type_ == RATGDO_CLIENT_ID) {
+                value = random(0x1, 0xFFFF);
+            } else {
                 value = 0;
             }
         }
         this->publish_state(value);
         this->control(value);
 
-        if (this->number_type_ == RATGDO_CLIENT_ID){
+        if (this->number_type_ == RATGDO_CLIENT_ID) {
             this->parent_->subscribe_client_id([=](uint32_t value) {
                 this->update_state(value);
             });
-        }else if (this->number_type_ == RATGDO_ROLLING_CODE_COUNTER) {
+        } else if (this->number_type_ == RATGDO_ROLLING_CODE_COUNTER) {
             this->parent_->subscribe_rolling_code_counter([=](uint32_t value) {
                 this->update_state(value);
             });
@@ -75,9 +75,9 @@ namespace ratgdo {
 
     void RATGDONumber::control(float value)
     {
-        if (this->number_type_ == RATGDO_CLIENT_ID){
+        if (this->number_type_ == RATGDO_CLIENT_ID) {
             this->parent_->set_rolling_code_counter(value);
-        }else if (this->number_type_ == RATGDO_ROLLING_CODE_COUNTER) {
+        } else if (this->number_type_ == RATGDO_ROLLING_CODE_COUNTER) {
             this->parent_->set_rolling_code_counter(value);
         } else if (this->number_type_ == RATGDO_OPENING_DURATION) {
             this->parent_->set_opening_duration(value);
