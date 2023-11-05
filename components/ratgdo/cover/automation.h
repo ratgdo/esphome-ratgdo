e2f4b6ec5f53654b19fd0b9a4e1ff01cbb3f2969
Marius Muja <mariusmuja@gmail.com>
2023-11-05 11:49:59 -0800

    Adding cover on_opening and on_closing triggers (#81)
    Co-authored-by: J. Nick Koston <nick@koston.org>

---
+++ components/ratgdo/cover/automation.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "esphome/components/cover/cover.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace ratgdo {
+
+    class CoverOpeningTrigger : public Trigger<> {
+    public:
+        CoverOpeningTrigger(cover::Cover* a_cover)
+        {
+            a_cover->add_on_state_callback([this, a_cover]() {
+                if (a_cover->current_operation == cover::COVER_OPERATION_OPENING) {
+                    this->trigger();
+                }
+            });
+        }
+    };
+
+    class CoverClosingTrigger : public Trigger<> {
+    public:
+        CoverClosingTrigger(cover::Cover* a_cover)
+        {
+            a_cover->add_on_state_callback([this, a_cover]() {
+                if (a_cover->current_operation == cover::COVER_OPERATION_CLOSING) {
+                    this->trigger();
+                }
+            });
+        }
+    };
+
+} // namespace ratgdo
+} // namespace esphome
