1617fac39f9f6e968c22a0c20fbc4b5acad59ab0
Paul Wieland <p@ul.sickdimension.com>
2024-08-01 09:09:02 -0400

    Update observable.h

--- components/ratgdo/observable.h
+++ components/ratgdo/observable.h
@@ -2,6 +2,7 @@
 #include <functional>
 #include <utility>
 #include <vector>
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace ratgdo {
@@ -31,10 +32,13 @@ namespace ratgdo {
         void subscribe(Observer&& observer)
         {
             this->observers_.push_back(std::forward<Observer>(observer));
+            ESP_LOGD("XXX","The observers vector is %d in length",this->observers_->size());
         }
 
         void notify() const
         {
+            ESP_LOGD("YYY","NOTIFY %d subscribers",this->observers_->size());
+
             for (const auto& observer : this->observers_) {
                 observer(this->value_);
             }
