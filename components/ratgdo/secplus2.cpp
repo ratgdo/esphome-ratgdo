c1d073a92b72db72db74e8c7d1fb24ece6cb3106
Paul Wieland <4663918+PaulWieland@users.noreply.github.com>
2026-06-05 17:47:10 -0400

    Update secplus2.cpp
    If execution reaches line 112, we already know synced is false. Therefore, the && !synced check is completely redundant.

--- components/ratgdo/secplus2.cpp
+++ components/ratgdo/secplus2.cpp
@@ -109,7 +109,7 @@ namespace secplus2 {
             return;
         }
 
-        if (tries == 2 && !synced) { // made a few attempts and no progress
+        if (tries == 2) { // made a few attempts and no progress
             // increment rolling code counter by some amount in case we crashed without writing to flash the latest value
             this->increment_rolling_code_counter(MAX_CODES_WITHOUT_FLASH_WRITE);
         }
